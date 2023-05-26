ea808032b46ea3a3d241c90c1c6df4d32f40bc71
Anteneh Tilaye <antenehtilaye19@gmail.com>
2023-05-26 16:03:07 +0300

    init: repo

---
+++ src/main/java/com/black/esms_boot/EsmsBootApplication.java
@@ -0,0 +1,13 @@
+package com.black.esms_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmsBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/black/esms_boot/EsmsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.esms_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmsBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
